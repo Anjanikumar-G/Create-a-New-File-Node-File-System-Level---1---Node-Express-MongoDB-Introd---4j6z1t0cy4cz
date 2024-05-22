bef18c0df05524f03b68ec6b32001513d23ab07b
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-22 12:26:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try {
+    await fs.writeFile(fileName, fileContent);
+    console.log('File written successfully');
+  } catch (error) {
+    console.error('Error writing the file:', error);
+  }
 };
 
 module.exports = { writeFile };
